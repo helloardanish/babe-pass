3ffcf766e7e737f5580c98d4fa4941c5a9a0ff15
A R Danish <helloardanish@gmail.com>
2025-02-28 17:54:58 +0530

    primeng library support added

---
+++ docs/commands.md
@@ -0,0 +1,5 @@
+[PrimeNG-The Next-Gen UI Suite for Angular](https://primeng.org/)
+
+```sh
+npm install primeng @primeng/themes
+```
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {ButtonModule} from 'primeng/button';
+import {InputMask} from 'primeng/inputmask';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    ButtonModule,
+    InputMask,
+    FormsModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'babe-pass';
+  phoneNumber: string = '';
 }
